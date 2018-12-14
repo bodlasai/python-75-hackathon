5d612d2beb6d1e8b25fd77f5fc7dd8641dad2339
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-14 21:08:48 +0530

    Create vote.py

---
+++ vote.py
@@ -0,0 +1,8 @@
+#basic if condation
+#to check for eligibility for voting
+
+age=int(input("enter your age:"))
+if(age>=18):
+    print(age,":eligiable for voting")
+else:
+    print(age,":not eligable for voting")
