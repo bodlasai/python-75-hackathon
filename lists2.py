8a3e3f2957f2575198087bd2d8493c77aa6be0bf
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-14 21:51:49 +0530

    Create lists2.py

---
+++ lists2.py
@@ -0,0 +1,12 @@
+#different oprations on lists
+l=[1,2,3,4,5]
+l.append(6)
+print(l)
+l.remove(l[3])
+print(l)
+l.insert(1,10)
+print(l)
+l.append("sai")
+l.insert(0,"hello list")
+print(l)
+print(len(l[0]))
