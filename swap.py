e7aeaf325500da7a469adfd060636582c71fe2a8
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-14 21:37:19 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,9 @@
+a = int(input("enter the a value:"))
+b= int(input("enter the b value:"))
+
+a=a+b
+b=a-b
+a=a-b
+print("after swapping the values are:")
+print("the value of a is:",a)
+print("the value of b is :",b)
