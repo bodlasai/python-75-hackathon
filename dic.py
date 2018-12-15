5cea8a41727e08d78c6a0b8516899b63315062e6
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-15 17:43:43 +0530

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,12 @@
+first={
+    "name":"sai",
+    "college":"sathyabama",
+    "present year":"3rd",
+    "pgm":"B.E-CSE"
+}
+#to display all the values
+for x,y in first.items():
+    print(x,y)
+#chaniging the value
+first["name"]="ram"
+print(first
