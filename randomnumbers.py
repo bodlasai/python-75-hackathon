5711aeabe557059906df169dfa43df7790aed578
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-15 18:00:28 +0530

    Update randomnumbers.py

--- randomnumbers.py
+++ randomnumbers.py
@@ -4,3 +4,10 @@ for x in range(1,10):
 print("function on random numbers")
 print(random.choice([10,20,30,10]))
 print(random.randrange(20, 50))
+
+a=[1,2,3,4,5]
+random.shuffle(a)
+for i in range(0,len(a)):
+    print(a[i])
+
+print(random.uniform(2,5))
