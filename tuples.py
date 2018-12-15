11e83469ede9a6bd6862afd1093b81db10d37490
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-15 17:23:37 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -8,3 +8,10 @@ print(type(n1)
 n2=("sai",[1,2,3],(2,3,4))
 print(n2[0][0])
 print(n2[1][0])
+#slicing
+n3=('a','e','i','o','u')
+print(n3[1::2])
+print(n3[:-1])
+#adding two tiples
+n4=(1,2,3)+(4,5,6)
+print(n4)
