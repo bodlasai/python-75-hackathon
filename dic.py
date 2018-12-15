31ef4b4f13c8b2022ee964d4785cbe81a44a0a3c
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-15 17:44:06 +0530

    Update dic.py

--- dic.py
+++ dic.py
@@ -9,4 +9,10 @@ for x,y in first.items():
     print(x,y)
 #chaniging the value
 first["name"]="ram"
-print(first
+print(first)
+#printing the terms
+for x in first:
+    print(x)
+#printing the values
+for x in first.values():
+    print(x)
