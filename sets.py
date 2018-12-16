bac4b920ba7a65edf2ec8821e949849192f3d90d
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-16 16:49:32 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,10 @@
+set={"sai","ram","chanikya","vikram"}
+print(set)
+#add an element to set
+set.add("hello every one")
+print(set)
+#to find the length
+print(len(set))
+#to updat the entire set
+set.update({"welcome","happ to see u","take care","sai","ram","ola","uber"})
+print(set)
