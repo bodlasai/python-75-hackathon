e18aa7109630a4874a46ac815151f7938cf31304
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-16 14:27:52 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,13 @@
+import  classandobject
+
+a=int(input("entert the a value"))
+b=int(input("enter the b value"))
+
+c=classandobject.add(a,b)
+print(c)
+d=classandobject.sub(a,b)
+print(d)
+e=classandobject.mul(a,b)
+print(e)
+f=classandobject.div(a,b)
+print(f)
