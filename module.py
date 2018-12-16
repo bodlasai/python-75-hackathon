3a37e3bcb4369f8a49a5af0a6a98ce2d2e6f4830
bodla sai <30978046+bodlasai@users.noreply.github.com>
2018-12-16 14:29:02 +0530

    Update module.py

--- module.py
+++ module.py
@@ -11,3 +11,20 @@ e=classandobject.mul(a,b)
 print(e)
 f=classandobject.div(a,b)
 print(f)
+
+#crete seperate file for the follwing
+class computer:
+    def config(self):
+        print("1tb,6gb ram,hello")
+
+a=computer()
+computer.config(a)
+a.config()
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
